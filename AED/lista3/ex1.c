c1d31f5e69edf1c95ec6011d1c8711d049dd8187
Migueldsbatista <msb2@cesar.school>
2024-09-04 12:08:50 -0300

    mudancas

---
+++ AED/lista3/ex1.c
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node{ 
+int data; 
+struct Node *next; 
+struct Node *prev; 
+} Node;
+void insertFirst(Node **head, int value);
+
+void insertOrdered(Node **head, int value);
+
+int main(){
+
+return 0;
+}
+
+void insertOrdered(Node **head, int value){
+    Node *newNode=(Node*)malloc(sizeof(Node));
+    newNode->data=value;
+    newNode->next=NULL;
+    newNode->prev=NULL;
+
+    if(*head==NULL){//lista vazia
+        *head=newNode;
+        return;
+    }
+
+    Node *current=*head;
+    // Caso o novo valor deva ser inserido no início da lista
+    if(current->data > value){
+        newNode->next=current;
+        
+        current->prev=newNode;
+
+        *head=newNode;
+    }
+
+
+    // Encontrar a posição correta para inserir
+
+    // Encontrar a posição correta para inserir
+
+    // Inserir o novo nó na lista
+
+
+    /*    if (current->next != NULL) {
+        current->next->prev = newNode;
+    }
+
+    current->next = newNode;*/
+
+}
+
+void insertFirst(Node **head, int value){
+    Node *newNode=(Node*)malloc(sizeof(Node));
+    newNode->data=value;
+    newNode->next=*head;//proximo é a nossa head atual
+    newNode->prev=NULL;//sempre null pois é o primeiro elemento
+
+    if(*head!=NULL){
+        (*head)->prev=newNode;
+    }
+    *head=newNode;
+    
+}
