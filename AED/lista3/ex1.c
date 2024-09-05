a54bdae0e17f50f40d1649ebba8a3695ce54593f
Miguel Batista <msb2@cesar.school>
2024-09-04 23:10:12 -0300

    novo algoritimo do banheiro

--- AED/lista3/ex1.c
+++ AED/lista3/ex1.c
@@ -17,40 +17,40 @@ return 0;
 }
 
 void insertOrdered(Node **head, int value){
-    Node *newNode=(Node*)malloc(sizeof(Node));
-    newNode->data=value;
-    newNode->next=NULL;
-    newNode->prev=NULL;
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
 
-    if(*head==NULL){//lista vazia
-        *head=newNode;
+    if(*head == NULL){  // Lista vazia
+        *head = newNode; 
         return;
     }
 
-    Node *current=*head;
+    Node *current = *head;
+
     // Caso o novo valor deva ser inserido no início da lista
     if(current->data > value){
-        newNode->next=current;
-        
-        current->prev=newNode;
-
-        *head=newNode;
+        newNode->next = current;
+        current->prev = newNode;
+        *head = newNode;
+        return;
     }
 
-
     // Encontrar a posição correta para inserir
-
-    // Encontrar a posição correta para inserir
+    while(current->next != NULL && current->next->data < value){
+        current = current->next;
+    }
 
     // Inserir o novo nó na lista
+    newNode->next = current->next;
+    newNode->prev = current;
 
-
-    /*    if (current->next != NULL) {
+    if(current->next != NULL) {
         current->next->prev = newNode;
     }
 
-    current->next = newNode;*/
-
+    current->next = newNode;
 }
 
 void insertFirst(Node **head, int value){
@@ -63,5 +63,66 @@ void insertFirst(Node **head, int value){
         (*head)->prev=newNode;
     }
     *head=newNode;
-    
 }
+
+Node* intercalar(Node *head1, Node *head2){
+    Node *head3=NULL;
+    while (head1 || head1)
+    {
+        if(head1){
+            insertFirst(&head3, head1->data);
+            head1=head1->next;
+
+        }
+        if(head2){
+            insertFirst(&head3, head2->data);
+            head2=head2->next;
+
+        }
+    }
+    return head3;
+}
+
+void printNode(Node *head){
+    while(head){
+        printf("%d", head->data);
+        head=head->next;
+    }
+}
+
+void searchElement(Node *head, int value){
+    int encontrado = 0;
+    int before = 0;
+    int after = 0;
+
+    if(head == NULL){  // Lista vazia
+        printf("0 elementos antes.\n0 elementos depois.\n"); 
+        return;
+    }
+
+    // Encontrar o valor na lista
+    while(head != NULL){
+        if(head->data == value){
+            encontrado = 1;
+            break;
+        }
+        head = head->next;
+        before++;
+    }
+
+    if(encontrado){
+        Node *temp = head->next; // Posição do próximo elemento
+        while(temp != NULL){
+            after++;
+            temp = temp->next;
+        }
+        printf("%d elementos antes.\n%d elementos depois.\n", before, after);
+    } else {
+        printf("%d elementos antes.\n0 elementos depois.\n", before);
+    }
+}
+
+
+
+    // Inserir o novo nó na lista
+
---
+++ IS/monitors.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define PESSOAS_APERTADAS 5
+
+typedef struct {
+    pthread_mutex_t chaveDoBanheiro;//Encapsulamento (Banheiro vai estar bem trancado)
+    int senhaAbencoada;
+} Banheiro;
+
+void abrirBanheiro(Banheiro *banheiro) {
+    pthread_mutex_init(&banheiro->chaveDoBanheiro, NULL);
+    banheiro->senhaAbencoada = 0;
+}
+
+void entrarNoBanheiro(Banheiro *banheiro) {//lock
+    pthread_mutex_lock(&banheiro->chaveDoBanheiro);//hora de iniciar as necessidades 💩
+}
+
+void sairDoBanheiro(Banheiro *banheiro) {
+    pthread_mutex_unlock(&banheiro->chaveDoBanheiro);//hora do alivio
+}
+
+void matarEncanador(Banheiro *banheiro) {
+    pthread_mutex_destroy(&banheiro->chaveDoBanheiro);//hora de acabar com a vida do encanador
+}
+
+void* pessoaNoBanheiro(void* arg) {
+    Banheiro *banheiro = (Banheiro*)arg;
+
+    usleep(rand() % 500000);
+
+    entrarNoBanheiro(banheiro);
+
+    banheiro->senhaAbencoada++;
+    printf("Pessoa %d entrou no banheiro e pegou a chave para fazer suas necessidades! senhaAbencoada: %d\n", pthread_self(), banheiro->senhaAbencoada);
+
+    usleep(2000000);//💩💩💩💩💩
+
+    printf("Pessoa %d terminou e está aliviada! Senha do 💩: %d\n", pthread_self(), banheiro->senhaAbencoada);
+
+    sairDoBanheiro(banheiro);
+
+    return NULL;
+}
+
+int main() {
+    Banheiro banheiro;
+    pthread_t pessoas[PESSOAS_APERTADAS];//Numero de pessoas apertadas
+
+    abrirBanheiro(&banheiro);
+
+    //Recepcionista do banheiro
+    for(int i = 0; i < PESSOAS_APERTADAS; ++i) {
+        //numero de pessoas que vao usar o banheiro | pessoas não apertadas(NULL)| Oq a pessoa vai fazer no banheiro | O banheiro que a pessoa vai fazer suas necessidades
+        pthread_create(&pessoas[i], NULL, pessoaNoBanheiro, &banheiro);
+    }
+
+    // Espera pelas pessoas terminarem terminarem de usar o banheiro
+    for(int i = 0; i < PESSOAS_APERTADAS; ++i) {
+        pthread_join(pessoas[i], NULL);
+    }
+
+    matarEncanador(&banheiro);
+    return 0;
+}
