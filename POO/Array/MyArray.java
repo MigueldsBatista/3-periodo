72152d97ee8c5a7b8596c9da506ac0d7f3f0f166
Migueldsbatista <msb2@cesar.school>
2024-09-19 12:11:47 -0300

    muitaas mudanças

---
+++ POO/Array/MyArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MyArray {
+    double d[] = new double[] {1.2, 1.3, 1.4};
+
+    public static void main(String[] args) {
+        double[] d = {1.2, 1.3, 1.4};
+        // Usando Arrays.toString para imprimir o array
+        System.out.println("Array: " + Arrays.toString(d));
+        System.out.println(d);
+
+        
+    }
+}
