9916d6e496fb8279f76c063ae9e4819f7ed979d8
Migueldsbatista <msb2@cesar.school>
2024-09-26 10:03:57 -0300

    merge sort implemented only in code

--- AED/sorting/quick.c
+++ AED/sorting/quick.c
@@ -1,51 +1,14 @@
 #include <stdio.h>
 
-/* 
- * Função de ordenação quicksort
- *
- * Parâmetros:
- * arr[]: o array que será ordenado
- * left: o índice esquerdo do array
- * right: o índice direito do array
- */
-void quickSort(int arr[], int left, int right) {
-    // Variáveis para percorrer o array
-    int i = left, j = right;
-    // Variável temporária para troca de elementos
-    int tmp;
-    // Definindo o pivô como o elemento central do array
-    int pivot = arr[(left + right) / 2];
-    
-    // Particionamento do array
-    while (i <= j) {
-        // Avança o índice 'i' até encontrar um elemento maior ou igual ao pivô
-        while (arr[i] < pivot)
-            i++;
-        // Regride o índice 'j' até encontrar um elemento menor ou igual ao pivô
-        while (arr[j] > pivot)
-            j--;
-        // Se os índices ainda não se cruzaram, trocamos os elementos
-        if (i <= j) {
-            // Troca os elementos arr[i] e arr[j]
-            tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            // Move os índices
-            i++;
-            j--;
-        }
-    }
-        
-    // Recursão para a sublista da esquerda
-    if (left < j) {
-        quickSort(arr, left, j);
-    }
+//Re
+int partition(int arr[], int startIndex, int endIndex);
 
-    // Recursão para a sublista da direita
-    if (i < right) {
-        quickSort(arr, i, right);
-    }
-}
+void quickSort(int arr[], int startIndex, int endIndex);
+
+void swap(int arr[], int i, int j);
+
+
+
 
 int main() {
     // Definição e inicialização do array
@@ -62,3 +25,39 @@ int main() {
 
     return 0;
 }
+
+int partition(int arr[], int startIndex, int endIndex) {
+    int pivot, pivotIndex;
+    pivot=arr[startIndex];
+    pivotIndex=endIndex;
+
+    for(int i= endIndex; i > startIndex;i--) {
+        if(arr[i] >= pivot) {
+            swap(arr, i , pivotIndex);
+            pivotIndex--;
+    }
+
+    }
+    swap(arr, startIndex, pivotIndex);
+    return pivotIndex;
+}
+
+
+void quickSort(int arr[], int startIndex, int endIndex) {
+
+    if (startIndex > endIndex) return;
+
+    int pivot = partition(arr, startIndex, endIndex);
+    
+    quickSort(arr, startIndex, pivot - 1);
+    
+    quickSort(arr, pivot + 1, endIndex);
+
+    }
+
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
